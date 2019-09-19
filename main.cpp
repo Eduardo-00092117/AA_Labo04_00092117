eb82d4ba8a29410c692006075a00387fe19d4ab4
Eduardo López <>
2019-09-18 21:53:14 -0600

    Terminado

--- main.cpp
+++ main.cpp
@@ -1,23 +1,64 @@
-
 #include <cstdlib>
 #include <iostream>
 
 using namespace std;
 
-struct Nodo{
-    int dato;
-    Nodo *sig;
-    Nodo *prev;
-};
 
-void maximo(int T){
-    int A[] = {3, 0, 1};
-    int B[] = {3, 0, 1};
+
+void swap(int* a, int* b){
+    int t = *a;
+    *a = *b;
+    *b = t;
+}
+
+int partition(int arr[], int low, int hight){
+    int pivot = arr[hight];
+    int i = (low-1);
+    
+    for(int j = low; j <= hight-1;j++){
+        if(arr[j] < pivot){
+            i++;
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[hight]);
+    return (i+1);
+}
+
+void quickSort(int arr[], int low, int high){
+    if(low < high){
+        int pi = partition(arr, low, high);
+        
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void printArray(int arr[], int size){
+    int i;
+    for(i=0; i < size; i++){
+        cout << arr[i] << "";
+    }
+    cout << endl;
+}
+
+void maximo(int T, int n = 1){
     if(T != 0){
+        int prueba = 0;
+        cout << "El tamaño del arreglo " << n << ": ";
+        cin >> prueba;
+        prueba = prueba - 1;
+        int A[prueba];
+        cout << "El arreglo " << n << endl;
         int temp = 0;
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i <= prueba; i++){
+            std::cin >> A[i];
+        }
+        quickSort(A, 0, prueba);
+        
+        for(int i = 0; i <= prueba; i++){
             if(A[i]%2 == 1){
-                if(i == 2){
+                if(i == prueba){
                     temp = temp = A[i]+1;
                 } else{
                     if(A[i+1]%2 == 1){
@@ -26,54 +67,22 @@ void maximo(int T){
                 }
             }
         }
-        cout << temp << endl;
-        maximo(T-1);
+        cout << "El dato que falta del arreglo " << n << "es " << temp << endl;
+        maximo(T-1, n + 1);
     }
 }
 
 int main(int argc, char** argv) {
-
-    /*ListaDoble L1;
     
-    L1.agregarElemento(1);
-    L1.agregarElemento(2);
-    L1.agregarElemento(3);
-    L1.agregarElemento(4);
-    L1.mostrarListaAdelante();
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int hola;
     
-    L1.liberar(2);
+    int T;
     
-    L1.mostrarListaAdelante();*/
+    cout << "Cuantos casos quiere probar: ";
+    cin >> T;
     
-    /*ListaDoble L1;
-    
-    int A[] = {1, 3, 3, 3, 6, 7};
-    
-    for(int i = 0; i <6; i++){
-        cout << A[i] << endl;
-    }
-    
-    for(int i = 0; i <6; i++){
-        if((A[i]%2) == 1){
-            if((i) == 5){
-                if(A[i]+1 != 2){
-                    L1.agregarElemento(A[i]+1);
-                }
-            } else{
-                if((A[i+1]%2) != 0){
-                    if(A[i]+1 != 2){
-                        L1.agregarElemento(A[i]+1);
-                    }
-                } 
-            }
-        }
-    }
-    
-    L1.mostrarListaAdelante();*/
-    
-    maximo(2);
-    
-   
+    maximo(T); 
     
     return 0;
 }
