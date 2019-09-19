b22708dee8ed79964bdf55c1a3f1c9a3f8632272
Eduardo López <>
2019-09-18 20:55:21 -0600

    Haciendo el ejercicio de otro modo

--- main.cpp
+++ main.cpp
@@ -10,96 +10,26 @@ struct Nodo{
     Nodo *prev;
 };
 
-class ListaDoble{
-  
-    private:
-        Nodo *inicio;
-        Nodo *fin;
-    
-    public:
-        Nodo* crearNodo(int valor){
-            Nodo *n = new Nodo;
-            n -> dato = valor;
-            n -> sig = nullptr;
-            n -> prev = nullptr;
-            return n;
-        }
-        
-        void agregarElemento(int valor){
-            Nodo *n = crearNodo(valor), *temp;
-            if(!inicio){
-                inicio = n;
-                fin = n;
-            } else{
-                fin -> sig = n;
-                n -> prev = fin;
-                fin = n;
-            }
-        }
-        
-        void mostrarListaAdelante(){
-            Nodo *temp = inicio;
-            if(!inicio){
-                cout << "La lista no posee elementos" << endl;
-            } else{
-                while(temp -> sig){
-                    cout << temp -> dato << ", ";
-                    temp = temp -> sig;
+void maximo(int T){
+    int A[] = {3, 0, 1};
+    int B[] = {3, 0, 1};
+    if(T != 0){
+        int temp = 0;
+        for(int i = 0; i < 3; i++){
+            if(A[i]%2 == 1){
+                if(i == 2){
+                    temp = temp = A[i]+1;
+                } else{
+                    if(A[i+1]%2 == 1){
+                        temp = A[i]+1;
+                    }
                 }
-                cout << temp -> dato;
-                cout << endl;
             }
         }
-        
-        void mostrarListaAtras(){
-            Nodo *temp = fin;
-            if(!inicio){
-                cout << "La lista no posee elementos" << endl;
-            } else{
-                while(temp){
-                    cout << temp -> dato << ", ";
-                    temp = temp -> prev;
-                }
-                
-                cout << endl;
-            }
-        }
-        
-        quitarNodo(Nodo *temp){
-            
-            
-            if(inicio == temp){
-                inicio = temp->sig;
-            }
-            
-            if(temp -> sig){
-                temp -> sig -> prev = temp -> prev;
-            }
-            
-            if(temp -> prev){
-                temp -> prev -> sig = temp -> sig;
-            }
-            
-            free(temp);
-        }
-        
-        liberar(int n){
-            Nodo *temp = inicio;
-            if(temp -> dato == n){
-                cout << "Es el primer elemento de la lista por lo que no se puede eliminar el anterior" << endl;
-            } else{
-                for(; temp && temp -> dato != n; temp = temp -> sig);
-                quitarNodo(temp);
-            }
-        }
-        
-        ListaDoble(){
-            inicio = nullptr;
-            fin = nullptr;
-        }
-    
-    
-};
+        cout << temp << endl;
+        maximo(T-1);
+    }
+}
 
 int main(int argc, char** argv) {
 
@@ -115,7 +45,7 @@ int main(int argc, char** argv) {
     
     L1.mostrarListaAdelante();*/
     
-    ListaDoble L1;
+    /*ListaDoble L1;
     
     int A[] = {1, 3, 3, 3, 6, 7};
     
@@ -126,16 +56,22 @@ int main(int argc, char** argv) {
     for(int i = 0; i <6; i++){
         if((A[i]%2) == 1){
             if((i) == 5){
-                L1.agregarElemento(A[i]+1);
+                if(A[i]+1 != 2){
+                    L1.agregarElemento(A[i]+1);
+                }
             } else{
                 if((A[i+1]%2) != 0){
-                    L1.agregarElemento(A[i]+1);
+                    if(A[i]+1 != 2){
+                        L1.agregarElemento(A[i]+1);
+                    }
                 } 
             }
         }
     }
     
-    L1.mostrarListaAdelante();
+    L1.mostrarListaAdelante();*/
+    
+    maximo(2);
     
    
     
